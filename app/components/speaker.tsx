e625f0aeacb902e7145d22e40d3b0c8cb1bad0a0
Daniel Castro <2968031+odanieldcs@users.noreply.github.com>
2024-11-16 12:33:48 -0300

    feat: updated speaker component

--- app/components/speaker.tsx
+++ app/components/speaker.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from './avatar';
 export type SpeakerProps = {
 	name: string;
 	company: string;
@@ -14,13 +15,9 @@ function SpeakerTitle({ role, company }: { role: string; company: string }) {
 function Speaker({ name, company, role, linkedin, avatar }: SpeakerProps) {
 	return (
 		<div className="flex space-x-2 items-center">
-			<img
-				src={avatar}
-				alt={name}
-				className="rounded-full max-w-16 max-h-16 border-[3px] border-white"
-			/>
+			<Avatar name={name} imageUri={avatar} />
 			<div className="flex flex-col">
-				<h3 className="font-medium leading-5 tracking-wide text-primary">
+				<h3 className="font-medium leading-5 -tracking-wide text-primary">
 					{name}
 				</h3>
 				<SpeakerTitle role={role} company={company} />
