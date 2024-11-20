07b323a7e521e54cf320c29e5dc54dc92ec85132
Daniel Castro <2968031+odanieldcs@users.noreply.github.com>
2024-11-19 23:27:06 -0300

    feat: add custom background image for hero banner in Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ export default {
 	content: ['./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'hero-banner': "url('/hero-banner.jpg')",
+			},
 			fontFamily: {
 				sans: [
 					'Poppins',
